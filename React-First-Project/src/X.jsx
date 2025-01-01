5f3b7136bbedd065ebbd08beca30f3b541441af6
moayad-barakat <moayadmohammed77@gmail.com>
2025-01-02 00:20:33 +0300

    second commit

--- React-First-Project/src/X.jsx
+++ React-First-Project/src/X.jsx
@@ -52,6 +52,56 @@ function Container() {
             {/* End Header */}
 
 
+             {/* Start section 3 */}
+             <div className="section-3">
+                <div>
+                    <img className="section3-img" src={man} alt="" />
+                </div>
+
+                <div className="section3-text">
+                    <h1>Best Organic Fruits And Vegetables</h1>
+                    <p className="text1-section3">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet
+
+
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, quos! Fugiat, laboriosam. Consectetur, odio veniam!
+                    </p>
+                    <ul className="section3-lists">
+                        <li><i class="fa-solid fa-check section3-icons"></i> <span>Tempor erat elitr rebum at clita</span></li>
+                        <li><i class="fa-solid fa-check section3-icons"></i> <span>Aliqu diam amet diam et eos</span></li>
+                        <li><i class="fa-solid fa-check section3-icons"></i> <span>Clita duo justo magna dolore erat amet</span></li>
+                    </ul>
+
+
+                    <button className="section3-btn">Read More</button>
+                </div>
+
+
+            </div>
+
+            {/* End Section 3 */}
+
+
+
+
+
+   {/* START FIRM SECTTION */}
+
+
+
+   <div className="firm-section">
+                <div>
+                    <h1>Visit Our Firm</h1>
+                    <p className="firm-text">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos.</p>
+
+                </div>
+
+                <div>
+                    <button className="firm-button">Visit Now</button>
+                </div>
+            </div>
+
+            {/* END FIRM SECTTION  */}
+
            
 
 
